004e9bcd48ae252de2afbd8bcd7e4792f32b10a7
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-04-20 10:44:05 +0500

    Remove temporary files after encoding

--- av1-cut.sh
+++ av1-cut.sh
@@ -15,7 +15,6 @@ VIDEO_FILE="$2.video.webm"
 ENCODING_OPTIONS="-c:v libaom-av1 -crf $CRF -b:v 0 -pix_fmt yuv420p -g 300"
 ffmpeg -ss $3 -i "$1" -t $4 $ENCODING_OPTIONS -pass 1 -an -sn -f null /dev/null && \
 ffmpeg -ss $3 -i "$1" -t $4 $ENCODING_OPTIONS -pass 2 -an -sn "$VIDEO_FILE"
-rm ffmpeg2pass*.log
 
 # Audio
 AUDIO_FILE="$2.audio.opus"
@@ -23,3 +22,6 @@ ffmpeg -ss $3 -i "$1" -t $4 -c:a $AUDIO_CODEC -b:a $AUDIO_BITRATE -ac 2 "$AUDIO_
 
 # Final video
 ffmpeg -i "$VIDEO_FILE" -i "$AUDIO_FILE" -c copy "$2"
+
+# Clean up
+rm ffmpeg2pass*.log "$VIDEO_FILE" "$AUDIO_FILE"
