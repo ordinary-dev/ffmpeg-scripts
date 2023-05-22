8d647c269e04af6d2031b3e6e8fe69e192ed62b8
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-05-22 17:44:14 +0500

    VP9: add "-ac 2" flag, move some parameters to a variable

--- vp9hq.sh
+++ vp9hq.sh
@@ -5,6 +5,8 @@
 #
 # Example: $ ./vp9hq.sh video.mp4 video.webm
 
-ffmpeg -i "$1" -c:v libvpx-vp9 -crf 20 -b:v 0 -pix_fmt yuv420p -pass 1 -an -f null /dev/null
-ffmpeg -i "$1" -c:v libvpx-vp9 -crf 20 -b:v 0 -pix_fmt yuv420p -pass 2 -c:a libopus -b:a 512k "$2"
+COMMON_PARAMS="-c:v libvpx-vp9 -crf 20 -b:v 0 -pix_fmt yuv420p"
+
+ffmpeg -i "$1" $COMMON_PARAMS -pass 1 -an -f null /dev/null
+ffmpeg -i "$1" $COMMON_PARAMS -pass 2 -c:a libopus -b:a 512k -ac 2 "$2"
 rm ffmpeg2pass*.log
